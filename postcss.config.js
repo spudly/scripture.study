9e97fcdb8638159f1d3ab75acbd0cf300023b38c
Stephen Sorensen <shuoink@gmail.com>
2020-03-04 20:20:28 -0500

    fix css in build?

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
+    content: ["./components/**/*.js", "./pages/**/*.tsx"],
     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
   }
 ];
